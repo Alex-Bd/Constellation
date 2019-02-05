a46d8e56ec4909bca40d9f587f28d77c6ea5f36d
Bódi <axel3425@hotmail.com>
2019-02-05 10:18:25 +0100

    Weather api and Tests fixes

--- src/test/java/db/constellation/neo/controllers/weather/WeatherRouterTest.java
+++ src/test/java/db/constellation/neo/controllers/weather/WeatherRouterTest.java
@@ -8,10 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasProperty;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
@@ -23,14 +19,14 @@ public class WeatherRouterTest {
     @Test
     public void weatherRouteTest(){
         assertEquals("Debrecen",restTemplate
-                .getForEntity("http://localhost:8070/Weather?city=Debrecen", CurrentWeatherEntity.class).getBody().getName()
-                );
+                .getForEntity("http://localhost:8070/Weather?city=Debrecen",
+                              CurrentWeatherEntity.class).getBody().getName());
 }
     @Test
     public void forecastRouteTest(){
 
-        assertEquals("Debrecen",restTemplate.getForEntity("http://localhost:8070/Forecast?city=Debrecen", ForecastWeatherEntity.class)
-                .getBody().getCity().getName());
+        assertEquals("Debrecen",restTemplate.getForEntity("http://localhost:8070/Forecast?city=Debrecen",
+                    ForecastWeatherEntity.class).getBody().getCity().getName());
 
     }
 
