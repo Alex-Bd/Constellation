0b893627cf47ef425fb6dfb67e6864f40a981455
Alex-Bodi <alex.bodi@hotmail.com>
2019-04-23 16:51:15 +0200

    PreFlight test

--- src/main/java/db/constellation/neo/controllers/music/MusicHandler.java
+++ src/main/java/db/constellation/neo/controllers/music/MusicHandler.java
@@ -18,11 +18,9 @@ class MusicHandler {
 
     Mono<ServerResponse> PreFlight(ServerRequest request) {
         return ServerResponse.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-
         .header("Access-Control-Allow-Origin", "*")
-        .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        .header("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with")
+        .header("Access-Control-Allow-Methods", "*")
+        .header("Access-Control-Allow-Headers", "*")
         .header("Access-Control-Max-Age", "3600")
                 .body(BodyInserters.empty());
     }
